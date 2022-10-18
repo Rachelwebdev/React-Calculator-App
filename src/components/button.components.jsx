98b6f9b0db31d8577efa9b93c77b2814902de5e2
Rachel Isaac <rachelisaac995@gmail.com>
2022-10-18 22:21:20 +0400

    Initial commit

---
+++ src/components/button.components.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./button.styles.css";
+
+const ButtonComponent = (props) => {
+  return (
+    <button
+      className="button-style"
+      onClick={() => props.handleClick(props.children)}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default ButtonComponent;
